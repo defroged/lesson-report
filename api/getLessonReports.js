7b06f7e301b65618799d9385a534b159ca644f6a
Ron Ward <defroged@gmail.com>
2025-05-19 10:10:01 +0900

    fix: Include audioURL in API response

--- api/getLessonReports.js
+++ api/getLessonReports.js
@@ -63,6 +63,7 @@ export default async function handler(req, res) {
         date: data.date.toDate().toLocaleDateString(),
         classPictures: data.classPictures || [],
         homeworkURL: data.homeworkURL || '',
+        audioURL: data.audioURL || '', // <<< ADD THIS LINE
         processedData: data.processedData || {},
       };
     });
